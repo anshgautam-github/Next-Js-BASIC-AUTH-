4a23cfc1f135237076c161ff21836fc98fc8f4d3
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-04-16 03:07:08 +0530

    Create page.ts

---
+++ src/app/profile/[id]/page.ts
@@ -0,0 +1,16 @@
+//this is for handling dynmic routes 
+//here profile/routes can be handeled
+//so we don't know what will come after profile/route 
+
+export default function UserProfile({params}: any) {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile</h1>
+            <hr />
+            <p className="text-4xl">Profile page 
+            <span className=" p-2 ml-2 rounded bg-orange-500 text-black">{params.id}</span>
+            </p>
+
+            </div>
+    )
+}
